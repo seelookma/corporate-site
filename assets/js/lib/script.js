aa9c8933f0b3601e594dd4995fc21e6a2aa0e0aa
taku_y <y.taku84@gmail.com>
2021-02-21 01:08:18 +0900

    record0003T SP PC　更新

--- assets/js/lib/script.js
+++ assets/js/lib/script.js
@@ -1,14 +1,17 @@
 $(function(){
     $('.nav_toggle').on('click', function () {
         $('.nav_toggle, .sp_globalNav').toggleClass('show');
+        $('.nav_toggle').toggleClass('white')
     });
     });
 
+
+    if (window.matchMedia( "(max-width: 768px)" ).matches) {
     $(function() {
         var height=$(".header").height();
         $("body").css("margin-top", height + 30);//10pxだけ余裕をもたせる
       });
-
+    }
       $(function () {
         var pagetop = $('.toTop');
     
