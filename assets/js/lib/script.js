3804f5f4c5c435344db94483b971d774308bf250
jcool-yanagita <yanagita@j-cool.fm>
2021-06-08 23:13:01 +0900

    mv作成

---
+++ assets/js/lib/script.js
@@ -0,0 +1,41 @@
+$(function(){
+    const $slider = $(".mv__slick");
+ 
+// 左右の透過の2周目ががたつく対応
+$slider.on("beforeChange", (event, slick, currentSlide, nextSlide) => {
+  $slider.find(".slick-slide").each((index, el) => {
+    const $this = $(el),
+      slickindex = $this.attr("data-slick-index");
+    if (nextSlide == slick.slideCount - 1 && currentSlide == 0) {
+      // 現在のスライドが最初のスライドでそこから最後のスライドに戻る場合
+      if (slickindex == "-1") {
+        // 最後のスライドに対してクラスを付与
+        $this.addClass("is-active-next");
+      } else {
+        // それ以外は削除
+        $this.removeClass("is-active-next");
+      }
+    } else if (nextSlide == 0) {
+      // 次のスライドが最初のスライドの場合
+      if (slickindex == slick.slideCount) {
+        // 最初のスライドに対してクラスを付与
+        $this.addClass("is-active-next");
+      } else {
+        // それ以外は削除
+        $this.removeClass("is-active-next");
+      }
+    } else {
+      // それ以外は削除
+      $this.removeClass("is-active-next");
+    }
+  });
+});
+ 
+$('.mv__slick').slick({
+    dots: true,
+    centerMode: true,
+    centerPadding: '60px',
+    speed: 1000,
+    slidesToShow: 1
+});
+});
